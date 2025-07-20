47a5ec90d9791cbf865c4229960f779fd2714462
soares36 <soaresvinicius128@gmail.com>
2025-07-20 08:56:56 -0400

    primeiro commit

---
+++ OlaMundo/Aula C/aula1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 25;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.3;
+    char letra = 'A';
+    char nome[20] = "Vinicius";
+    int _teste;
+    //int 3tezte;          // não se começa variáveis com números.
+   // int int;             //não se usa palavras reservadas. ex: int int;
+
+printf ("a idade de %s é %d\n", nome, idade); //printf(“%formato1 %formato2”, variável1, variável2);
+//%d: Imprime um inteiro no formato decimal.
+//%i: Equivalente a %d.
+//%f: Imprime um número de ponto flutuante no formato padrão.
+//%e: Imprime um número de ponto flutuante na notação científica.
+ //%c: Imprime um único caractere.
+ //%s: Imprime uma cadeia (string) de caracteres.
+
+
+}
