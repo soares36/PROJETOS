52a78af24ff64904fc8e67d4169180dcb7e98a83
soares36 <soaresvinicius128@gmail.com>
2025-07-20 09:04:47 -0400

    segundo commit

---
+++ SuperTrunfo/super_trunfo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+
+    char Estado;
+    char[20] Código;
+    char[20] Nome da Cidade;
+    int População;
+    float Aréa;
+    float PIB;
+    int Número de Pontos Turísticos;
+
+
+
+}
