455c8595761179bd53c072f83034df51602724e5
soares36 <soaresvinicius128@gmail.com>
2025-07-22 20:01:30 -0400

    aula2

---
+++ OlaMundo/Aula C/Aula2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+
+    int numero1, numero2;
+    int soma, subtracao, multiplicacao, divisao;
+
+    soma = numero1 + numero2;
+
+    subtracao = numero1 - numero2;
+
+    multiplicacao = numero1 * numero2;
+
+    divisao = numero1 / numero2;
+
+    printf("Digite o numero 1: \n");
+    scanf("%d", &numero1);
+
+    printf("Digite o numero 2: \n");
+    scanf("%d", numero2);
+
+    printf("A soma é: "%d"\n":, soma);
+
+
+}
