db8b44c01af956421070140411935f20064a7982
soares36 <soaresvinicius128@gmail.com>
2026-01-06 21:50:30 -0400

    aaula php

---
+++ PAGINAS HTML/welcome.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+
+    Welcome <?php echo $_POST["name"]; ?> <br>
+    Your email adress is: <?php echo $_POST["email"]; ?>
+    
+</body>
+</html>
